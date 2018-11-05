7ab5a340d04a26a082526d664e32fef30b4752dc
vladstojna <vlad.leic@gmail.com>
2018-11-05 19:07:58 +0000

    generalised triangular geometry

--- proj3/geometry.js
+++ proj3/geometry.js
@@ -6,21 +6,27 @@ class PlaneGeometry extends THREE.Geometry {
 	constructor(u, v, w, width, height, depth, udir=1, vdir=1, widthSegments=1, heightSegments=1) {
 		super();
 
-		var wPolySize = width / widthSegments;
-		var hPolySize = height/ heightSegments;
+		var wPolySize = width  / widthSegments;
+		var hPolySize = height / heightSegments;
 
 		// Constant position in w axis
 		var posW = depth/2;
 
-		// Push line
+		var posU;
+		var posV;
+
+		var hOffset;
+
+		var vector;
+
+		/* Push vertices */
 		for (let hp = 0; hp <= heightSegments; hp++) {
 			// Calculate vertex position on v axis
-			var posV = hp * hPolySize - height/2;
-			// Push column
+			posV = hp * hPolySize - height/2;
 			for (let wp = 0; wp <= widthSegments; wp++) {
 				// Calculate vertex position on u axis
-				var posU = wp * wPolySize - width/2;
-				var vector = new THREE.Vector3();
+				posU = wp * wPolySize - width/2;
+				vector = new THREE.Vector3();
 				// Set vertex coordinates appropriately
 				vector[u] = posU * udir;
 				vector[v] = posV * vdir;
@@ -31,39 +37,92 @@ class PlaneGeometry extends THREE.Geometry {
 		}
 
 		/* Push faces */
-		/* Push line */
 		for (let h = 0; h < heightSegments; h++) {
-			/* Push column */
+			/* offset to compensate going 1 line up */
+			hOffset = h * (widthSegments + 1);
 			for (let w = 0; w < widthSegments; w++) {
+				/* push two faces to create a square segment */
 				this.faces.push(
 					new THREE.Face3(
-						w   + h*(widthSegments+1),
-						w+1 + h*(widthSegments+1),
-						w+widthSegments+1 + h*(widthSegments+1)),
+						hOffset + w,
+						hOffset + (w + 1),
+						hOffset + (w + 1 + widthSegments)),
 					new THREE.Face3(
-						w+1 + h*(widthSegments+1),
-						w+widthSegments+2 + h*(widthSegments+1),
-						w+widthSegments+1 + h*(widthSegments+1))
+						hOffset + (w + 1),
+						hOffset + (w + 2 + widthSegments),
+						hOffset + (w + 1 + widthSegments))
 				);
 			}
 		}
+
 		this.computeFaceNormals();
-		this.computeVertexNormals();
 	}
 }
 
 /* Generic triangle geometry */
 class TriangleGeometry extends THREE.Geometry {
-	constructor(a, b, c) {
+	constructor(u, v, w, width, height, depth, udir=1, vdir=1, widthSegments=1) {
 		super();
 
-		/* Push three vertices */
-		this.vertices.push(a, b, c);
-		/* Push single triangular face */
-		this.faces.push(new THREE.Face3(0, 1, 2));
-		/* Compute normals */
+		var heightSegments = widthSegments;
+
+		var wPolySize = width  / widthSegments;
+		var hPolySize = height / heightSegments;
+
+		// Constant position in w axis
+		var posW = depth/2;
+
+		var posU;
+		var posV;
+
+		var tempWSeg;
+		var hOffset = 0;
+
+		var vector;
+
+		/* Push Vertices */
+		for (let hp = 0; hp <= heightSegments; hp++) {
+			// Calculate vertex position on v axis
+			posV     = hp * hPolySize - height/2;
+			tempWSeg = widthSegments - hp;
+			for (let wp = 0; wp <= tempWSeg; wp++) {
+				// Calculate vertex position on u axis
+				posU   = wp * wPolySize - width/2;
+				vector = new THREE.Vector3();
+				// Set vertex coordinates appropriately
+				vector[u] = posU * udir;
+				vector[v] = posV * vdir;
+				vector[w] = posW;
+				// Push vertex
+				this.vertices.push(vector);
+			}
+		}
+
+		/* Push faces */
+		for (let h = 0; h < heightSegments; h++) {
+			/* width segments get reduced when going up in height */
+			tempWSeg = widthSegments - h;
+			for (let w = 0; w < tempWSeg; w++) {
+				/* push lower face */
+				this.faces.push(
+					new THREE.Face3(
+						hOffset + w,
+						hOffset + (w + 1),
+						hOffset + (w + 1 + tempWSeg))
+				);
+				/* if face is interior, then push upper face to make a square */
+				if (w < tempWSeg-1 && h < heightSegments-1)
+					this.faces.push(
+						new THREE.Face3(
+							hOffset + (w + 1),
+							hOffset + (w + 2 + tempWSeg),
+							hOffset + (w + 1 + tempWSeg))
+					);
+			}
+			hOffset += tempWSeg + 1;
+		}
+
 		this.computeFaceNormals();
-		this.computeVertexNormals();
 	}
 }
 
@@ -134,17 +193,17 @@ class RightTriangularPrismGeometry extends THREE.Geometry {
 		this.mergeMesh(
 			new THREE.Mesh(
 				new TriangleGeometry(
-					new THREE.Vector3(-width/2, -height/2, -depth/2),
-					new THREE.Vector3(-width/2, -height/2,  depth/2),
-					new THREE.Vector3(-width/2,  height/2, -depth/2))));
+					'z', 'y', 'x',
+					depth, height, -width, 1, 1,
+					depthSegments, heightSegments)));
 
 		/* Right side TriangleGeometry */
 		this.mergeMesh(
 			new THREE.Mesh(
 				new TriangleGeometry(
-					new THREE.Vector3(width/2, -height/2, -depth/2),
-					new THREE.Vector3(width/2,  height/2, -depth/2),
-					new THREE.Vector3(width/2, -height/2,  depth/2))));
+					'y', 'z', 'x',
+					depth, height, width, height/depth, depth/height,
+					depthSegments, heightSegments)));
 
 		this.mergeVertices();
 	}
