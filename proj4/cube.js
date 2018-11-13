a1df06ea93db441321a57ac4ba0ec0c4905ae2b5
vladstojna <vlad.leic@gmail.com>
2018-11-13 16:10:53 +0000

    created simple cube

---
+++ proj4/cube.js
@@ -0,0 +1,34 @@
+/* Custom cube class */
+
+class Cube extends THREE.Object3D {
+
+	/* constructor */
+	constructor(width, height, depth,
+			widthSegments, heightSegments, depthSegments,
+			x=0, y=0, z=0,
+			shadedMaterial, basicMaterial,
+			texture=0) {
+
+		super();
+
+		this.position.set(x, y, z);
+
+		this.currentMaterial = shadedMaterial;
+		this.otherMaterial   = basicMaterial;
+
+		this.add(new THREE.Mesh(
+			new THREE.BoxGeometry(
+				width,
+				height,
+				depth,
+				widthSegments,
+				heightSegments,
+				depthSegments),
+			this.currentMaterial)
+		);
+
+	}
+
+	toggleShading(flag) {}
+	toggleWireframe(flag) {}
+}
