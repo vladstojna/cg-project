c8c7de1242ce26aef24786d6bcfb0a3e9624ff94
vladstojna <vlad.leic@gmail.com>
2018-11-13 16:11:22 +0000

    simple custom point light

---
+++ proj4/pointlight.js
@@ -0,0 +1,11 @@
+/* Custom point light class */
+
+class PointLight extends THREE.PointLight {
+
+	/* constructor */
+	constructor(color, intensity, distance, decay, x=0, y=0, z=0) {
+
+		super(color, intensity, distance, decay)
+		this.position.set(x, y, z);
+	}
+}
