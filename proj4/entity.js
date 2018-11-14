b4ce0466f9508edae21755e91b1f01946f67c6cc
vladstojna <vlad.leic@gmail.com>
2018-11-14 17:06:16 +0000

    generic game entity

---
+++ proj4/entity.js
@@ -0,0 +1,41 @@
+/* Generic game entity class */
+
+class GameEntity extends THREE.Object3D {
+	/* constructor */
+	constructor(shadedMaterial, basicMaterial) {
+		super();
+
+		this.currentMaterial = shadedMaterial;
+		this.otherMaterial   = basicMaterial;
+
+		this.wireframeFlag     = false;
+		this.shaderComputeFlag = true;
+	}
+
+	/* toggle wireframe */
+	toggleWireframe(wFlag) {
+		if (this.wireframeFlag != wFlag) {
+			this.traverse(node => {
+				if (node instanceof THREE.Mesh)
+					node.material.wireframe = wFlag;
+			});
+			this.wireframeFlag = wFlag;
+		}
+	}
+
+	/* toggles shader computing */
+	toggleShading(shaderFlag) {
+		if (this.shaderComputeFlag != shaderFlag)
+			var tempMaterial;
+			this.traverse(node => {
+				if (node instanceof THREE.Mesh)
+					node.material = this.otherMaterial;
+					node.material.wireframe = this.wireframeFlag;
+					/* swaps current with other material */
+					tempMaterial         = this.currentMaterial;
+					this.currentMaterial = this.otherMaterial;
+					this.otherMaterial   = tempMaterial;
+			});
+			this.shaderComputeFlag = shaderFlag;
+	}
+}
