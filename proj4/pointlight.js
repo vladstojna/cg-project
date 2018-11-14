036ad7092434e47dbad24adb5a08c5a8f787f072
vladstojna <vlad.leic@gmail.com>
2018-11-14 14:46:03 +0000

    added semicolon

--- proj4/pointlight.js
+++ proj4/pointlight.js
@@ -5,7 +5,7 @@ class PointLight extends THREE.PointLight {
 	/* constructor */
 	constructor(color, intensity, distance, decay, x=0, y=0, z=0) {
 
-		super(color, intensity, distance, decay)
+		super(color, intensity, distance, decay);
 		this.position.set(x, y, z);
 	}
 }
