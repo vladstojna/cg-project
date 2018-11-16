9137e54817855923cc8e59314579e4c26433e7f1
vladstojna <vlad.leic@gmail.com>
2018-11-16 17:05:22 +0000

    removed useless code

--- proj4/pause.js
+++ proj4/pause.js
@@ -6,12 +6,10 @@ class PauseMessage extends THREE.Object3D {
 	constructor(width, height, widthSegments=1, heightSegments=1, material, texture=0) {
 
 		super();
-		
-		this.visibilityFlag = true;
 
 		var tex;
 
-		this.position.y = 500;
+		this.visibilityFlag = true;
 
 		this.pausePlane = new THREE.Mesh(
 			new THREE.PlaneGeometry(
@@ -25,7 +23,6 @@ class PauseMessage extends THREE.Object3D {
 
 		tex = new THREE.TextureLoader().load(texture);
 		tex.anisotropy = 8;
-		tex.format = THREE.RGBFormat;
 		this.pausePlane.material.map = tex;
 		
 		this.toggleVisibility(this.visibilityFlag);
