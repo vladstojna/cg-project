289fdab80be3ee0bcc2c85e9885df848ca4cee4e
vladstojna <vlad.leic@gmail.com>
2018-11-17 23:24:39 +0000

    lights can now reset their state

--- proj4/direclight.js
+++ proj4/direclight.js
@@ -3,13 +3,19 @@
 class DirectionalLight extends THREE.DirectionalLight {
 
 	/* constructor */
-	constructor(color, intensity, x=0, y=0, z=0) {
+	constructor(color, intensity, x=0, y=0, z=0, initialState=true) {
 		super(color, intensity);
 		this.position.set(x, y, z);
+		this.initialState = initialState;
+		this.visible      = initialState;
 	}
 
 	switchLight(flag) {
 		this.visible = flag;
+	}
+
+	resetState() {
+		this.visible = this.initialState;
 	}
 
 }
--- proj4/pointlight.js
+++ proj4/pointlight.js
@@ -3,13 +3,19 @@
 class PointLight extends THREE.PointLight {
 
 	/* constructor */
-	constructor(color, intensity, distance, decay, x=0, y=0, z=0) {
+	constructor(color, intensity, distance, decay, x=0, y=0, z=0, initialState=true) {
 		super(color, intensity, distance, decay);
 		this.position.set(x, y, z);
+		this.initialState = initialState;
+		this.visible      = initialState;
 	}
 
 	switchLight(flag) {
 		this.visible = flag;
+	}
+
+	resetState() {
+		this.visible = this.initialState;
 	}
 
 }
